d39b34d183cb58920024740c98d2aefa6b060733
s7manth <syalamarty002@gmail.com>
2023-11-12 15:43:27 +0800

    change the topology construction code and format the file

--- topos/ring.py
+++ topos/ring.py
@@ -7,47 +7,49 @@ from mininet.cli import CLI
 from mininet.log import setLogLevel, info, debug
 from mininet.node import Host, RemoteController
 
+
 class TreeTopo(Topo):
   """
   Tree topology
   """
 
-  def __init__(self, infileName = "ring.in"):
+  def __init__(self):
     Topo.__init__(self)
-    self.infileName = infileName
-    
-    # Read input file
-    with open(infileName) as f:
-      lines = f.readlines()
-    
-    config = [int(x) for x in lines[0].split()]
-    self.nHosts = config[0]
-    self.nSwitches = config[1]
-    self.nLinks = config[2]
-    self.links = [tuple(x.split(',')) for x in lines[1:]]
 
   def build(self):
-    # Add hosts
-    for h in range(1, self.nHosts + 1):
-      host = self.addHost('h%d' % h)
-        
-    # Add switches 
-    for s in range(1, self.nSwitches + 1):
-      sconfig = {
-        'dpid': "%016x" % s
-      }
-      switch = self.addSwitch('s%d' % s, **sconfig)
-        
-    # Add links
-    for link in self.links:
+    infileName = "star.in"
+
+    with open(infileName, "r+") as f:
+      lines = f.readlines()
+
+    config = [int(x) for x in lines[0].split()]
+
+    nHosts = config[0]
+    nSwitches = config[1]
+    nLinks = config[2]
+
+    print(f"Number of Hosts: {nHosts}")
+    print(f"Number of Switches: {nSwitches}")
+    print(f"Number of Links: {nLinks}")
+
+    links = [tuple(x.strip().split(",")) for x in lines[1:]]
+
+    for h in range(1, nHosts + 1):
+      self.addHost("h%d" % h)
+
+    for s in range(1, nSwitches + 1):
+      sconfig = {"dpid": "%016x" % s}
+
+      self.addSwitch("s%d" % s, **sconfig)
+
+    for link in links:
       self.addLink(link[0], link[1])
-        
 
-topos = {
-  'sdnip' : (lambda: TreeTopo()) 
-}
 
-if __name__ == '__main__':
-  sys.path.insert(1, '/home/sdn/onos/topos')
+topos = {"sdnip": (lambda: TreeTopo())}
+
+if __name__ == "__main__":
+  sys.path.insert(1, "/home/sdn/onos/topos")
   from onosnet import run
+
   run(TreeTopo())
--- topos/star.py
+++ topos/star.py
@@ -7,6 +7,7 @@ from mininet.cli import CLI
 from mininet.log import setLogLevel, info, debug
 from mininet.node import Host, RemoteController
 
+
 class TreeTopo(Topo):
   """
   Tree topology
@@ -17,40 +18,38 @@ class TreeTopo(Topo):
 
   def build(self):
     infileName = "star.in"
-    
-    # Read input file
-    with open(infileName) as f:
+
+    with open(infileName, "r+") as f:
       lines = f.readlines()
-    
+
     config = [int(x) for x in lines[0].split()]
-    print("config", config)
+
     nHosts = config[0]
     nSwitches = config[1]
     nLinks = config[2]
-    links = [tuple(x.strip().split(',')) for x in lines[1:]]
 
-    # Add hosts
+    print(f"Number of Hosts: {nHosts}")
+    print(f"Number of Switches: {nSwitches}")
+    print(f"Number of Links: {nLinks}")
+
+    links = [tuple(x.strip().split(",")) for x in lines[1:]]
+
     for h in range(1, nHosts + 1):
-      host = self.addHost('h%d' % h)
-        
-    # Add switches 
+      self.addHost("h%d" % h)
+
     for s in range(1, nSwitches + 1):
-      sconfig = {
-        'dpid': "%016x" % s
-      }
-      switch = self.addSwitch('s%d' % s, **sconfig)
-        
-    # Add links
+      sconfig = {"dpid": "%016x" % s}
+
+      self.addSwitch("s%d" % s, **sconfig)
+
     for link in links:
       self.addLink(link[0], link[1])
-        
-                    
-topos = { 
-  'sdnip' : (lambda: TreeTopo()) 
-}
 
-if __name__ == '__main__':
-  sys.path.insert(1, '/home/sdn/onos/topos')
+
+topos = {"sdnip": (lambda: TreeTopo())}
+
+if __name__ == "__main__":
+  sys.path.insert(1, "/home/sdn/onos/topos")
   from onosnet import run
-  run(TreeTopo())
 
+  run(TreeTopo())
