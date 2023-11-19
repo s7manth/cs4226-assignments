791bb846aaf52e13a125ba9eaa61bfea55841d73
s7manth <syalamarty002@gmail.com>
2023-11-19 17:39:14 +0800

    format topo

--- assignment-2/A0226581A/topology.py
+++ assignment-2/A0226581A/topology.py
@@ -20,102 +20,109 @@ class Topology(Topo):
             "r310": "100.100.3.1/32",
             "r410": "100.100.4.1/32",
         }
-        
+
         # Nodes
+<<<<<<< Updated upstream
         r110, r120, r130, r210, r310, r410 = [self.addNode(name=n, loopback=l, cls=FRRRouter) for n, l in nodes_with_loopbacks.items()]
+=======
+        r110, r120, r130, r210, r310, r410 = [
+            self.addNode(name=n, loopback=l, cls=FRRRouter)
+            for n, l in nodes_with_loopbacks
+        ]
+>>>>>>> Stashed changes
 
         hosts = {
-            "h211": {
-                "ip": "10.2.1.1/24",
-                "droute": "via 10.2.1.254"
-            },
-            "h311": {
-                "ip": "10.3.1.1/24",
-                "droute": "via 10.3.1.254"
-            },
-            "h411": {
-                "ip": "10.4.1.1/25",
-                "droute": "via 10.4.1.126"
-            },
-            "h412": {
-                "ip": "10.4.1.129/25",
-                "droute": "via 10.4.1.254"
-            },
+            "h211": {"ip": "10.2.1.1/24", "droute": "via 10.2.1.254"},
+            "h311": {"ip": "10.3.1.1/24", "droute": "via 10.3.1.254"},
+            "h411": {"ip": "10.4.1.1/25", "droute": "via 10.4.1.126"},
+            "h412": {"ip": "10.4.1.129/25", "droute": "via 10.4.1.254"},
         }
 
         # Hosts
+<<<<<<< Updated upstream
         h211, h311, h411, h412 = [self.addHost(name=h, ip=details["ip"], defaultRoute=details["droute"]) for h, details in hosts.items()]
+=======
+        h211, h311, h411, h412 = [
+            self.addHost(name=h, ip=details["ip"], defaultRoute=details["droute"])
+            for h, details in hosts
+        ]
+>>>>>>> Stashed changes
 
         # Links (Between routers and hosts)
         self.addLink(
             r210,
-            h211, 
-            intfName1="r210-eth0", 
-            params1={ "ip": "10.2.1.254/24" },
+            h211,
+            intfName1="r210-eth0",
+            params1={"ip": "10.2.1.254/24"},
         )
         self.addLink(
-            r310, 
-            h311, 
-            intfName1="r310-eth0", 
-            params1={ "ip": "10.3.1.254/24" },
+            r310,
+            h311,
+            intfName1="r310-eth0",
+            params1={"ip": "10.3.1.254/24"},
         )
         self.addLink(
-            r410, 
-            h411, 
-            intfName1="r410-eth0", 
-            params1={ "ip": "10.4.1.126/25" },
+            r410,
+            h411,
+            intfName1="r410-eth0",
+            params1={"ip": "10.4.1.126/25"},
         )
         self.addLink(
-            r410, 
-            h412, 
-            intfName1="r410-eth1", 
-            params1={ "ip": "10.4.1.254/25" },
+            r410,
+            h412,
+            intfName1="r410-eth1",
+            params1={"ip": "10.4.1.254/25"},
         )
 
         # Links (Between routers)
         self.addLink(
-            r110, 
-            r120, 
-            intfName1="r110-eth1", 
-            intfName2="r120-eth1", 
-            params1={ "ip": "192.168.1.0/31" }, 
-            params2={ "ip": "192.168.1.1/31" },
+            r110,
+            r120,
+            intfName1="r110-eth1",
+            intfName2="r120-eth1",
+            params1={"ip": "192.168.1.0/31"},
+            params2={"ip": "192.168.1.1/31"},
         )
         self.addLink(
-            r110, 
+            r110,
             r210,
-            intfName1="r110-eth2", 
-            intfName2="r210-eth1", 
-            params1={ "ip": "172.17.1.0/31" },
-            params2={ "ip": "172.17.1.1/31" })
+            intfName1="r110-eth2",
+            intfName2="r210-eth1",
+            params1={"ip": "172.17.1.0/31"},
+            params2={"ip": "172.17.1.1/31"},
+        )
         self.addLink(
-            r130, 
-            r310,
-            intfName1="r130-eth2", 
-            intfName2="r310-eth1", 
-            params1={ "ip": "172.17.2.0/31" },
-            params2={ "ip": "172.17.2.1/31" })
-        self.addLink(
-            r110, 
-            r410,
-            intfName1="r110-eth3", 
-            intfName2="r410-eth2", 
-            params1={ "ip": "172.17.3.0/31" },
-            params2={ "ip": "172.17.3.1/31" })
-        self.addLink(
-            r120, 
             r130,
-            intfName1="r120-eth2", 
-            intfName2="r130-eth1", 
-            params1={ "ip": "192.168.1.2/31" },
-            params2={ "ip": "192.168.1.3/31" })
+            r310,
+            intfName1="r130-eth2",
+            intfName2="r310-eth1",
+            params1={"ip": "172.17.2.0/31"},
+            params2={"ip": "172.17.2.1/31"},
+        )
         self.addLink(
-            r130, 
+            r110,
             r410,
-            intfName1="r130-eth3", 
-            intfName2="r410-eth3", 
-            params1={ "ip": "172.17.4.0/31" },
-            params2={ "ip": "172.17.4.1/31" })
+            intfName1="r110-eth3",
+            intfName2="r410-eth2",
+            params1={"ip": "172.17.3.0/31"},
+            params2={"ip": "172.17.3.1/31"},
+        )
+        self.addLink(
+            r120,
+            r130,
+            intfName1="r120-eth2",
+            intfName2="r130-eth1",
+            params1={"ip": "192.168.1.2/31"},
+            params2={"ip": "192.168.1.3/31"},
+        )
+        self.addLink(
+            r130,
+            r410,
+            intfName1="r130-eth3",
+            intfName2="r410-eth3",
+            params1={"ip": "172.17.4.0/31"},
+            params2={"ip": "172.17.4.1/31"},
+        )
 
 
 def startNetwork():
